d78fb0cd7c82f1791ae473a60a4505a119dbb4bf
karina <karyna.rein@gmail.com>
2023-01-20 10:54:29 +0300

    решена задача в папке 6

---
+++ 6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Напишите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a % 2;
+
+if(b > 0)
+{
+    Console.Write ("нет");
+}
+else 
+{
+    Console.Write ("да");
+}
