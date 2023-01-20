6b62ef07057b9b3485c16ffa83dc37d450e0f53a
karina <karyna.rein@gmail.com>
2023-01-20 09:35:40 +0300

    решена задача в папке 2

---
+++ 2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Напишите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Напишите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+{
+    Console.Write("max = ");
+    Console.Write(a);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+}
